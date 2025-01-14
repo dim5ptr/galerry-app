040b205b91241558fc24d00e819615ac87ccf899
dim5ptr <dimasetya1836@gmail.com>
2025-01-14 10:28:16 +0700

    updated

--- src/hooks/usePhotoGalery.ts
+++ src/hooks/usePhotoGalery.ts
@@ -107,22 +107,6 @@ export interface UserPhoto {
   webviewPath?: string;
 }
 
-// export async function base64FromPath(path: string): Promise<string> {
-//   const response = await fetch(path);
-//   const blob = await response.blob();
-//   return new Promise((resolve, reject) => {
-//     const reader = new FileReader();
-//     reader.onerror = reject;
-//     reader.onload = () => {
-//       if (typeof reader.result === 'string') {
-//         resolve(reader.result);
-//       } else {
-//         reject('method did not return a string')
-//       }
-//     };
-//     reader.readAsDataURL(blob);
-//   });
-// }
 export async function base64FromPath(path: string): Promise<string> {
   const response = await fetch(path);
   const blob = await response.blob();
@@ -131,11 +115,12 @@ export async function base64FromPath(path: string): Promise<string> {
     reader.onerror = reject;
     reader.onload = () => {
       if (typeof reader.result === 'string') {
-        resolve(reader.result.split(',')[1]); // Mengambil data base64 tanpa prefix
+        resolve(reader.result);
       } else {
-        reject(new Error('Hasil tidak berupa string'));
+        reject('method did not return a string')
       }
     };
     reader.readAsDataURL(blob);
   });
 }
+
